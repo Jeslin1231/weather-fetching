82b62a6884d331bed62cbcc7902cc7b8d22177eb
Jeslin1231 <wwx970624771@gmail.com>
2023-05-09 18:36:15 -0700

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ca1e15046cmsh51d8848eb8486e5p122b6fjsn7b44b4788dc7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "e632783b1b6c6fcaa697509314f8579d"
